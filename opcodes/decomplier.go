133b096e47ae6609956dc957da31c8d7c24170fd
0x19 <nevio@eiger.co>
2023-06-16 21:04:48 +0200

    More docs for opcodes

--- opcodes/decomplier.go
+++ opcodes/decomplier.go
@@ -12,10 +12,14 @@ import (
 )
 
 var (
+	// ErrEmptyBytecode is an error that indicates the absence of bytecode.
 	ErrEmptyBytecode = errors.New("bytecode is not set or empty bytecode provided")
 )
 
-// Decompiler decompiles bytecode into optcode instructions.
+// Decompiler decompiles bytecode into opcode instructions.
+// The bytecode to be decompiled can be set using the SetBytecode() method.
+// Decompiling is performed by the Decompile() method. The results can be
+// obtained using GetInstructions(), String(), and other related methods.
 type Decompiler struct {
 	ctx          context.Context
 	bytecode     []byte
@@ -23,26 +27,52 @@ type Decompiler struct {
 	instructions []Instruction
 }
 
-// Instruction represents an optcode instruction.
+// Instruction represents an opcode instruction. It contains the offset for the
+// instruction, the opcode itself and the arguments for the opcode, if any.
 type Instruction struct {
 	Offset int
 	OpCode OpCode
 	Args   []byte
 }
 
+// SetBytecode sets the bytecode that the decompiler should work on. It also
+// updates the bytecode size.
+// Example:
+//
+//	d := opcodes.NewDecompiler(context.Background(), bytecode)
+//	d.SetBytecode(anotherBytecode)
 func (d *Decompiler) SetBytecode(b []byte) {
 	d.bytecode = b
 	d.bytecodeSize = uint64(len(b))
 }
 
+// GetBytecode returns the bytecode that the decompiler is working on.
+// Example:
+//
+//	d := opcodes.NewDecompiler(context.Background(), bytecode)
+//	fmt.Println(d.GetBytecode())
 func (d *Decompiler) GetBytecode() []byte {
 	return d.bytecode
 }
 
+// GetBytecodeSize returns the size of the bytecode that the decompiler is working on.
+// Example:
+//
+//	d := opcodes.NewDecompiler(context.Background(), bytecode)
+//	fmt.Println(d.GetBytecodeSize())
 func (d *Decompiler) GetBytecodeSize() uint64 {
 	return d.bytecodeSize
 }
 
+// Decompile decompiles the bytecode into opcode instructions. This must be
+// called before any information can be retrieved from the decompiler. It
+// returns an error if the bytecode is empty.
+// Example:
+//
+//	d := opcodes.NewDecompiler(context.Background(), bytecode)
+//	if err := d.Decompile(); err != nil {
+//	  log.Fatal(err)
+//	}
 func (d *Decompiler) Decompile() error {
 	if d.bytecodeSize < 1 {
 		return ErrEmptyBytecode
@@ -74,6 +104,12 @@ func (d *Decompiler) Decompile() error {
 }
 
 // MatchFunctionSignature checks if a given function signature matches any of the decompiled instructions.
+// The function signature should be the Keccak-256 hash of the function signature.
+// Example:
+//
+//	d := opcodes.NewDecompiler(context.Background(), bytecode)
+//	d.Decompile()
+//	fmt.Println(d.MatchFunctionSignature("0xa9059cbb"))
 func (d *Decompiler) MatchFunctionSignature(signature string) bool {
 	// Remove "0x" prefix if present
 	signature = strings.TrimPrefix(signature, "0x")
@@ -89,7 +125,12 @@ func (d *Decompiler) MatchFunctionSignature(signature string) bool {
 	return false
 }
 
-// GetCallInstructions returns all instructions related to the CALL opcode.
+// GetInstructionsByOpCode returns all instructions related to the specified opcode.
+// Example:
+//
+//	d := opcodes.NewDecompiler(context.Background(), bytecode)
+//	d.Decompile()
+//	instructions := d.GetInstructionsByOpCode(opcodes.PUSH1)
 func (d *Decompiler) GetInstructionsByOpCode(op OpCode) []Instruction {
 	var callInstructions []Instruction
 	for _, instruction := range d.instructions {
@@ -100,12 +141,22 @@ func (d *Decompiler) GetInstructionsByOpCode(op OpCode) []Instruction {
 	return callInstructions
 }
 
-// GetInstructions returns the decompiled optcode instructions.
+// GetInstructions returns the decompiled opcode instructions.
+// Example:
+//
+//	d := opcodes.NewDecompiler(context.Background(), bytecode)
+//	d.Decompile()
+//	instructions := d.GetInstructions()
 func (d *Decompiler) GetInstructions() []Instruction {
 	return d.instructions
 }
 
 // String returns a string representation of the decompiled bytecode as a sequence of bytes.
+// Example:
+//
+//	d := opcodes.NewDecompiler(context.Background(), bytecode)
+//	d.Decompile()
+//	fmt.Println(d)
 func (d *Decompiler) String() string {
 	var buf bytes.Buffer
 
@@ -126,11 +177,24 @@ func (d *Decompiler) String() string {
 }
 
 // IsOpCode checks if the given instruction represents the specified opcode.
+// Example:
+//
+//	d := opcodes.NewDecompiler(context.Background(), bytecode)
+//	d.Decompile()
+//	instructions := d.GetInstructions()
+//	if len(instructions) > 0 {
+//	  fmt.Println(d.IsOpCode(instructions[0], opcodes.PUSH1))
+//	}
 func (d *Decompiler) IsOpCode(instruction Instruction, op OpCode) bool {
 	return instruction.OpCode == op
 }
 
 // OpCodeFound checks if the specified opcode is found in any of the decompiled instructions.
+// Example:
+//
+//	d := opcodes.NewDecompiler(context.Background(), bytecode)
+//	d.Decompile()
+//	fmt.Println(d.OpCodeFound(opcodes.PUSH1))
 func (d *Decompiler) OpCodeFound(op OpCode) bool {
 	for _, instruction := range d.instructions {
 		if instruction.OpCode == op {
@@ -140,6 +204,12 @@ func (d *Decompiler) OpCodeFound(op OpCode) bool {
 	return false
 }
 
+// GetOpCodesAsString returns the opcode part of the decompiled instructions as a slice of strings.
+// Example:
+//
+//	d := opcodes.NewDecompiler(context.Background(), bytecode)
+//	d.Decompile()
+//	fmt.Println(d.GetOpCodesAsString())
 func (d *Decompiler) GetOpCodesAsString() []string {
 	var opCodes []string
 	for _, instruction := range d.instructions {
@@ -149,15 +219,34 @@ func (d *Decompiler) GetOpCodesAsString() []string {
 }
 
 // IsSelfDestruct checks if the decompiled instructions contain the SELFDESTRUCT opcode.
+// Example:
+//
+//	d := opcodes.NewDecompiler(context.Background(), bytecode)
+//	d.Decompile()
+//	fmt.Println(d.IsSelfDestruct())
 func (d *Decompiler) IsSelfDestruct() bool {
 	return d.OpCodeFound(SELFDESTRUCT)
 }
 
-// isControlFlowInstruction checks if the given opcode represents a control flow instruction.
+// IsControlFlowInstruction checks if the given opcode represents a control flow instruction.
+// Example:
+//
+//	d := opcodes.NewDecompiler(context.Background(), bytecode)
+//	d.Decompile()
+//	fmt.Println(d.IsControlFlowInstruction(opcodes.JUMP))
 func (d *Decompiler) IsControlFlowInstruction(op OpCode) bool {
 	return op == JUMP || op == JUMPI || op == JUMPDEST || op == RETURN || op == REVERT || op == SELFDESTRUCT
 }
 
+// MatchInstruction checks if the given instruction exists in the decompiled instructions.
+// Example:
+//
+//	d := opcodes.NewDecompiler(context.Background(), bytecode)
+//	d.Decompile()
+//	instructions := d.GetInstructions()
+//	if len(instructions) > 0 {
+//	  fmt.Println(d.MatchInstruction(instructions[0]))
+//	}
 func (d *Decompiler) MatchInstruction(instruction Instruction) bool {
 	for _, inst := range d.instructions {
 		if inst.Offset == instruction.Offset && inst.OpCode == instruction.OpCode && bytes.Equal(inst.Args, instruction.Args) {
@@ -167,8 +256,15 @@ func (d *Decompiler) MatchInstruction(instruction Instruction) bool {
 	return false
 }
 
-// NewDecompiler creates a new Decompiler instance.
-// @TODO: Add automatic decompile in the future from NewDecompiler()
+// NewDecompiler creates a new Decompiler instance with the provided context and bytecode.
+// The bytecode is not automatically decompiled; Decompile() must be called before any
+// information can be retrieved from the decompiler.
+// Example:
+//
+//	d := opcodes.NewDecompiler(context.Background(), bytecode)
+//	if err := d.Decompile(); err != nil {
+//	  log.Fatal(err)
+//	}
 func NewDecompiler(ctx context.Context, b []byte) (*Decompiler, error) {
 	return &Decompiler{
 		ctx:          ctx,
--- opcodes/tree.go
+++ opcodes/tree.go
@@ -6,12 +6,20 @@ import (
 )
 
 // TreeNode represents a node in the execution flow tree.
+// Each node represents an instruction, and has zero or more children.
+// These children represent subsequent instructions in the execution flow.
 type TreeNode struct {
 	Instruction Instruction
 	Children    []*TreeNode
 }
 
-// GetTree returns the tree representation of the execution flow.
+// GetTree returns the root of the tree representation of the execution flow.
+// The root of the tree is not a real instruction; its children are the real
+// start of the execution flow.
+// If there are no instructions, it returns nil.
+// Example:
+//
+//	tree := d.GetTree()
 func (d *Decompiler) GetTree() *TreeNode {
 	if len(d.instructions) == 0 {
 		return nil
@@ -25,7 +33,8 @@ func (d *Decompiler) GetTree() *TreeNode {
 	return root.Children[0]
 }
 
-// buildExecutionTree recursively builds the execution flow tree starting from the given node.
+// buildExecutionTree builds the execution flow tree from the given stack of nodes.
+// This is a private method used by GetTree().
 func (d *Decompiler) buildExecutionTree(stack []*TreeNode) {
 	for _, instruction := range d.instructions {
 		node := &TreeNode{Instruction: instruction}
@@ -44,25 +53,34 @@ func (d *Decompiler) buildExecutionTree(stack []*TreeNode) {
 }
 
 // PrintTree prints the tree representation of the execution flow.
+// Each node is indented according to its depth in the tree.
+// Example:
+//
+//	d.PrintTree()
 func (d *Decompiler) PrintTree() {
 	tree := d.GetTree()
 	d.printExecutionTree(tree, 0)
 }
 
-// printExecutionTree prints the execution flow tree.
-func (d *Decompiler) printExecutionTree(node *TreeNode, indent int) {
-	fmt.Printf("%sOffset: 0x%04x, OpCode: %s, Args: %x\n", strings.Repeat(" ", indent*2), node.Instruction.Offset, node.Instruction.OpCode.String(), node.Instruction.Args)
-	for _, child := range node.Children {
-		d.printExecutionTree(child, indent+1)
-	}
-}
-
 // PrintInstructionTree prints the execution flow tree for a specific instruction.
+// The root of the tree is the given instruction, and the children are the subsequent instructions.
+// Each node is indented according to its depth in the tree.
+// Example:
+//
+//	d.PrintInstructionTree(instruction)
 func (d *Decompiler) PrintInstructionTree(instruction Instruction) {
 	tree := d.getInstructionTree(instruction)
 	d.printExecutionTree(tree, 0)
 }
 
+// GetInstructionTreeFormatted returns a string representation of the execution flow tree for a specific instruction.
+// The root of the tree is the given instruction, and the children are the subsequent instructions.
+// Each node is indented according to its depth in the tree.
+// The indent string is prepended to each line.
+// Example:
+//
+//	s := d.GetInstructionTreeFormatted(instruction, "  ")
+//	fmt.Println(s)
 func (d *Decompiler) GetInstructionTreeFormatted(instruction Instruction, indent string) string {
 	var builder strings.Builder
 	builder.WriteString(fmt.Sprintf("%s0x%04x %s\n", indent, instruction.Offset, instruction.OpCode.String()))
@@ -76,6 +94,11 @@ func (d *Decompiler) GetInstructionTreeFormatted(instruction Instruction, indent
 }
 
 // GetChildrenByOffset returns the child instructions of a given offset.
+// These are the instructions that directly follow the instruction at the given offset.
+// If no such instructions exist, it returns an empty slice.
+// Example:
+//
+//	children := d.GetChildrenByOffset(offset)
 func (d *Decompiler) GetChildrenByOffset(offset int) []Instruction {
 	var children []Instruction
 	for _, instr := range d.instructions {
@@ -89,7 +112,18 @@ func (d *Decompiler) GetChildrenByOffset(offset int) []Instruction {
 	return children
 }
 
-// getInstructionTree returns the execution flow tree for a specific instruction.
+// printExecutionTree prints a given node and its children in the execution flow tree.
+// This is a private method used by PrintTree() and PrintInstructionTree().
+func (d *Decompiler) printExecutionTree(node *TreeNode, indent int) {
+	fmt.Printf("%sOffset: 0x%04x, OpCode: %s, Args: %x\n", strings.Repeat(" ", indent*2), node.Instruction.Offset, node.Instruction.OpCode.String(), node.Instruction.Args)
+	for _, child := range node.Children {
+		d.printExecutionTree(child, indent+1)
+	}
+}
+
+// getInstructionTree returns the root of the tree representation of the execution flow for a specific instruction.
+// The root of the tree is the given instruction, and the children are the subsequent instructions.
+// This is a private method used by PrintInstructionTree() and GetInstructionTreeFormatted().
 func (d *Decompiler) getInstructionTree(instruction Instruction) *TreeNode {
 	root := &TreeNode{Instruction: instruction}
 	offset := instruction.Offset
@@ -100,7 +134,8 @@ func (d *Decompiler) getInstructionTree(instruction Instruction) *TreeNode {
 	return root
 }
 
-// buildInstructionTree recursively builds the execution flow tree for a specific instruction.
+// buildInstructionTree builds the execution flow tree for a specific instruction from the given node.
+// This is a private method used by getInstructionTree().
 func (d *Decompiler) buildInstructionTree(node *TreeNode, offset int, visited map[int]bool) {
 	for _, instr := range d.instructions {
 		if instr.Offset == offset && !visited[offset] {
